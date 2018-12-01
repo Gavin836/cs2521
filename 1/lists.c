d345e1970c31c85bcc43a2ace58dda4fae3aa920
Gavin Wang <z5206647@student.unsw.edu.au>
2018-12-01 11:35:51 +1100

    lab01 finished

--- 1/lists.c
+++ 1/lists.c
@@ -73,6 +73,21 @@ int list_sum_items (link list)
 /** Frees all memory used in the list */
 void list_drop (link list)
 {
+    // Segment circular lists
+    link head = list;
+   
+    while (list->next != NULL) {
+        
+        if (list->next == head) {
+            list->next = NULL;
+            break;
+        }
+        
+        list = list->next;
+    }
+    
+    // Drop sequential lists
+    list = head;
     link new_head = list->next;
     
     while (list != NULL) {
@@ -89,33 +104,186 @@ void list_drop (link list)
 
 /** Create a circular list with the specified number of nodes,
  * with each link storing data from 1 to the number of nodes. */
-link clist_new (int n_nodes __unused)
+link clist_new (int n_nodes)
 {
-	return NULL;
+    if (n_nodes == 0) {
+        return NULL;
+    }
+    
+    if (n_nodes == 1) {
+        link new = node_new(1);
+        new->next = new;
+        
+        return new;
+    }
+    
+    link new = node_new(1);
+    link head = new;
+    Item it = 2;
+    
+    for (int i = 1; i < n_nodes; i++) {
+        
+        //Find tail
+        while (new->next != NULL) {            
+            new = new->next;
+        }
+        
+        new->next = node_new(it);
+      
+        it++;
+    }
+    
+    new->next->next = head;
+    
+	return head;
 }
 
 /** print the data in a circular fashion starting from any node */
-void clist_print (link clist __unused)
+void clist_print (link clist)
 {
-	return;
+    if (clist == NULL) {
+        printf("Empty\n");
+    
+    } else {
+    
+        link head = clist;
+        printf ("[%d]->", clist->item);
+        
+        clist = clist->next;    
+        
+        while(head != clist){
+            printf ("[%d]->", clist->item);
+            clist = clist->next;
+        }
+        puts(" Loop");
+    }
 }
 
 
 /** Create a double-linked list which contains the same values,
  * in the same order as 'list' */
-dlink dlist_new_from_list (link list __unused)
+dlink dlist_new_from_list (link list)
 {
-	return NULL;
+    // Empty list
+    if (list == NULL) {
+        return NULL;
+    }
+    
+    // Find no of links;
+    link link_head = list;
+    int no_links = 0;
+    
+    while (list != NULL) {
+        no_links++;
+        list = list->next;
+    }
+    
+    //Create chain
+    dlink new_dlink = NULL;
+    dlink dlink_prev = NULL;
+    dlink dlink_head = NULL;
+   
+    list = link_head;
+    Item it = list->item;
+    
+    for (int i = 0; i < no_links; i++) {
+
+        // First link vs. last link vs. intermediate links
+        if (i == 0) {
+            new_dlink = malloc(sizeof(dnode));
+            new_dlink->next = NULL;
+            new_dlink->prev = NULL;
+            new_dlink->item = it;
+            
+            dlink_head = new_dlink;
+            dlink_prev = new_dlink;
+        
+        } else if (i == (no_links - 1)) {
+            new_dlink->next = malloc(sizeof(dnode));
+            new_dlink = new_dlink->next;
+            new_dlink->next = NULL;
+            new_dlink->prev = dlink_prev;
+            new_dlink->item = it;
+            
+            
+        
+        } else {
+            new_dlink->next = malloc(sizeof(dnode));
+            new_dlink = new_dlink->next;
+            new_dlink->next = NULL;
+            new_dlink->prev = dlink_prev;
+            new_dlink->item = it;
+            
+            dlink_prev = new_dlink;
+        }
+       
+        if (list->next != NULL) {        
+            list = list->next;
+            it = list->item;
+        }
+        
+    }
+    
+    list = link_head;
+    list_drop(list);
+    
+	return dlink_head;
 }
 
 /** Print a doubly-linked list. */
-void dlist_print (dlink list __unused)
+void dlist_print (dlink list)
 {
-	return;
+    if (list == NULL) {
+    
+    } else if (list->next == NULL) {
+        printf("|->");
+	    printf ("[%d]->", list->item);
+	    puts("|");
+    
+    } else {
+    
+        dlink curr = list;
+        dlink temp = list->next;
+	    while (temp != NULL) {
+		    printf ("[%d]->", curr->item);
+            
+            curr = temp;
+            temp = temp->next;
+            
+	    }
+	    printf("[%d]->", curr->item);
+	
+        curr = curr->prev;
+        printf("(Reverse)->");
+        
+        while (curr != NULL) {
+            printf ("[%d]->", curr->item);
+            curr = curr->prev;
+        }
+        
+    	puts("|");
+
+    }
+
 }
 
 /** Frees all the memory used in the double-linked list */
-void dlist_drop (dlink list __unused)
+void dlist_drop (dlink list)
 {
-	return;
+    assert(list != NULL);
+    
+    if (list->next == NULL) {
+        free(list);
+    
+    } else {
+        list = list->next;
+        
+        while (list->next != NULL) {
+            free(list->prev);
+            list = list->next;
+        }
+        
+        free(list->prev);
+        free(list);
+    }
 }
--- 1/test_lists.c
+++ 1/test_lists.c
@@ -13,6 +13,16 @@ void test_one(link list);
 void test_two(link list);
 void test_three(link list);
 
+void test_circular0(int no);
+void test_circular1(int no);
+void test_circular2(int no);
+void test_circular3(int no);
+
+void create_dlink_zero (void);
+void create_dlink_one (void);
+void create_dlink_two (void);
+void create_dlink_three (void);
+
 int main (void)
 {
 	// Creates an empty list, and then prints it.
@@ -20,13 +30,23 @@ int main (void)
 	list_print (list);
 
 	// You should write some tests here. (And remove this comment.)
-    
-    
+        
     test_zero(list);
     test_one(list);
     test_two(list);
     test_three(list);
     
+    test_circular0(0);
+    test_circular1(1);
+    test_circular2(2);
+    test_circular3(3);
+    
+    create_dlink_zero();
+    create_dlink_one();
+    create_dlink_two();
+    create_dlink_three();
+    
+    printf("Tests successful!\n");      
 	return EXIT_SUCCESS;
 }
 
@@ -82,4 +102,106 @@ void test_three (link list) {
     list_print (list);
     assert(list_sum_items(list) == 6);
     list_drop(list);   
+}
+
+void test_circular0(int no){
+    clist_print(NULL);
+    assert(clist_new(no) == NULL);
+}
+void test_circular1(int no){
+    link circular1 = clist_new (no);
+    
+    clist_print(circular1);
+    assert(circular1->item == 1);
+    assert(circular1->next == circular1);
+    list_drop(circular1);
+}
+
+void test_circular2(int no){
+    link circular2 = clist_new(no);
+    
+    clist_print(circular2);
+    assert(circular2->item == 1);
+    assert(circular2->next->item == 2);
+    assert(circular2->next->next == circular2);
+    list_drop(circular2);
+}
+
+void test_circular3(int no){
+    link circular3 = clist_new(no);
+    
+    clist_print(circular3);
+    assert(circular3->item == 1);
+    assert(circular3->next->item == 2);
+    assert(circular3->next->next->item == 3);
+    assert(circular3->next->next->next == circular3);
+    list_drop(circular3);
+}
+
+void create_dlink_zero (void) {
+    dlist_print(NULL);
+    assert(dlist_new_from_list(NULL) == NULL);
+}
+
+void create_dlink_one (void) {
+    Item item_one = 1;
+    link list = node_new(item_one);
+    
+    dlink new_dlist = dlist_new_from_list(list);
+    
+    dlist_print(new_dlist);
+    assert(new_dlist->item == 1);
+    assert(new_dlist->next == NULL);
+    assert(new_dlist->prev == NULL);
+    
+    dlist_drop(new_dlist);
+}
+
+void create_dlink_two (void) {
+    link list = NULL;
+    Item item_one = 1;
+    link new_node = node_new(item_one);
+    list = new_node;
+    
+    Item item_two = 2;
+    new_node = node_new(item_two);
+    list_insert_next(list, new_node);
+    
+    dlink new_dlist = dlist_new_from_list(list);
+    
+    dlist_print(new_dlist);
+    assert(new_dlist->item == 1);
+    assert(new_dlist->prev == NULL);
+    assert(new_dlist->next->item == 2);
+    assert(new_dlist->next->prev == new_dlist);
+    assert(new_dlist->next->next == NULL);
+    
+    dlist_drop(new_dlist);
+}
+void create_dlink_three (void) {
+    link list = NULL;
+    Item item_one = 1;
+    link new_node = node_new(item_one);
+    list = new_node;
+    
+    Item item_two = 2;
+    new_node = node_new(item_two);
+    list_insert_next(list, new_node);
+   
+    Item item_three = 3;
+    new_node = node_new(item_three);
+    list_insert_next(list, new_node);
+    
+    dlink new_dlist = dlist_new_from_list(list);
+    
+    dlist_print(new_dlist);
+    assert(new_dlist->item == 1);
+    assert(new_dlist->prev == NULL);
+    assert(new_dlist->next->item == 3);
+    assert(new_dlist->next->prev == new_dlist);
+    assert(new_dlist->next->next->item == 2);
+    assert(new_dlist->next->next->next == NULL);
+    assert(new_dlist->next->next->prev == new_dlist->next);
+    
+    dlist_drop(new_dlist);
 }
