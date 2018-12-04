25a48f6c3b6536621a966d064792ee530af7d9a2
Gavin Wang <z5206647@student.unsw.edu.au>
2018-12-04 11:12:39 +1100

    struct implementation

--- ass1/test_textbuffer.c
+++ ass1/test_textbuffer.c
@@ -1,15 +1,27 @@
 ////////////////////////////////////////////////////////////////////////
 // COMP2521 19T0 ... assignment 1: Textbuffer ADT
 //
-// yyyy-mm-dd	Your Name Here <z5555555@unsw.edu.au>
+// 2018-12-02	Gavin Wang <z5206647@unsw.edu.au>
 
 #include <assert.h>
 #include <stdlib.h>
 
 #include "textbuffer.h"
 
+void test_textbuffer_new (void);
+
 int main (void)
 {
 	// your tests go here!
+	//Create testbuffer tests
+    test_testbuffer_new();
+	
+	
 	return EXIT_SUCCESS;
+}
+
+void test_textbuffer_new(void) {
+    assert(textbuffer_new("") == NULL);
+    assert(textbuffer_new("Single\n") != NULL);
+    assert(textbuffer_new("Double\nDouble\n") != NULL
 }
--- ass1/textbuffer.c
+++ ass1/textbuffer.c
@@ -1,8 +1,33 @@
 ////////////////////////////////////////////////////////////////////////
 // COMP2521 19T0 ... assignment 1: Textbuffer ADT
 //
-// yyyy-mm-dd	Your Name Here <z5555555@unsw.edu.au>
+// 2018-12-02	Gavin Wang  <z5206647@unsw.edu.au>
 
 #include "textbuffer.h"
 
 // your implementation goes here.
+typedef struct sentence *Sentence;
+typedef struct sentence {
+      Sentence next;
+      Sentence prev;
+      char *string;
+}sentence;
+
+typedef struct text_buffer {
+    Sentence head;
+    Sentence tail;
+    int no_sentences; 
+
+}text_buffer;
+
+
+
+Textbuffer textbuffer_new (const char *text){
+
+}
+
+void textbuffer_drop (Textbuffer tb){
+
+}
+
+
