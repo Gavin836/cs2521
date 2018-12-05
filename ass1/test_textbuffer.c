13b19be5cc79a29f6a046f9fbc17742889dcec14
Gavin Wang <z5206647@student.unsw.edu.au>
2018-12-05 22:48:07 +1100

    new and drop functions added. Tests for both

--- ass1/test_textbuffer.c
+++ ass1/test_textbuffer.c
@@ -5,23 +5,79 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "textbuffer.h"
 
-void test_textbuffer_new (void);
+static void test_textbuffer_empty (void);
+static void test_textbuffer_one (void);
+static void test_textbuffer_two (void);
 
 int main (void)
 {
-	// your tests go here!
-	//Create testbuffer tests
-    test_testbuffer_new();
+    //whitebox_tests();
+    
+	//Testcases for basic operations using a single textbuffer
+    test_textbuffer_empty();
+	test_textbuffer_one();
+	test_textbuffer_two();
 	
+	//Test not conclude on /n
+	puts("All tests passed!");
 	
 	return EXIT_SUCCESS;
 }
 
-void test_textbuffer_new(void) {
-    assert(textbuffer_new("") == NULL);
-    assert(textbuffer_new("Single\n") != NULL);
-    assert(textbuffer_new("Double\nDouble\n") != NULL
+
+static void test_textbuffer_empty(void) {
+    printf("Empty buffer test");
+    Textbuffer new;
+    //size_t no_lines;
+    //size_t no_bytes;
+    
+    new = textbuffer_new("");
+    //no_lines = textbuffer_lines(new);
+    //no_bytes = textbuffer_lines(new);
+    
+    assert(new == NULL);
+    puts(" -passed!");
 }
+
+static void test_textbuffer_one (void){
+    printf("Single newline buffer test");
+    Textbuffer new;
+    //size_t no_lines;
+    //size_t no_bytes;
+    
+    new = textbuffer_new("\n");
+    //no_lines = textbuffer_lines(new);
+    //no_bytes = textbuffer_lines(new);
+    
+    assert(new != NULL);
+    //assert(no_lines == 1);
+    //assert(no_bytes == 1);
+    
+    textbuffer_drop(new);
+    puts(" -passed!");
+}
+
+static void test_textbuffer_two (void){
+    printf("Two newlines buffer test");
+    Textbuffer new;
+    //size_t no_lines;
+    //size_t no_bytes;
+    
+    new = textbuffer_new("\n\n");
+    //no_lines = textbuffer_lines(new);
+    //no_bytes = textbuffer_lines(new);
+   
+    assert(new != NULL);
+    //assert(no_lines == 2);
+    //assert(no_bytes == 2);
+       
+    textbuffer_drop(new);
+    puts(" -passed!");
+}
+
+
--- ass1/textbuffer.c
+++ ass1/textbuffer.c
@@ -3,9 +3,11 @@
 //
 // 2018-12-02	Gavin Wang  <z5206647@unsw.edu.au>
 
+#include <stdio.h>
+#include <assert.h>
 #include "textbuffer.h"
 
-// your implementation goes here.
+//Structs implmentation
 typedef struct sentence *Sentence;
 typedef struct sentence {
       Sentence next;
@@ -13,21 +15,168 @@ typedef struct sentence {
       char *string;
 }sentence;
 
-typedef struct text_buffer {
+typedef struct textbuffer {
     Sentence head;
     Sentence tail;
     int no_sentences; 
+}textbuffer;
 
-}text_buffer;
+//Prototypes
+void add_sentence(Textbuffer tb, char *string);
 
-
+static void whitebox_tests (void);
+static void wtest_one (void);
+static void wtest_two (void);
+static void wtest_three (void);
 
 Textbuffer textbuffer_new (const char *text){
+    int i;
+    
+    //Catch empty strings or no new lines.
+    if (strcmp(text, "") == 0) {
+        puts("Empty string. Please input text");
+        return NULL;
+    
+    } else {
+        i = 0;
+        
+        while (1) {
+            
+            if (text[i] == '\0') {
+                puts("No newline character. Please try again");
+                return NULL;
+                
+            } else if (text[i] == '\n') {
+                break;
+            }
+            
+            i++;           
+        }    
+    }
+    
+    //Initialise textbuffer and it's attributes
+    Textbuffer tb_new = calloc(1, sizeof(textbuffer));
+    char *string = strdup(text);
+    char *copy = string;
+    char *string_sep = strsep(&string, "\n");
+    
+    //String_buf used to prevent the trailing "" from being included
+    char *string_buf = strsep(&string, "\n");
+    char *string_buf2;
+        
+    while ((string_buf2 = strsep(&string, "\n")) != NULL) {
+        add_sentence(tb_new, string_sep);
+        string_sep = string_buf;
+        string_buf = string_buf2;
+    }
+    
+    add_sentence(tb_new, string_sep);
+      
+    free(copy);
+    
+    return tb_new;
+}
 
+void add_sentence(Textbuffer tb, char *string) {
+    
+    // Produce "sentence" struct
+    char *temp = strdup(string);
+    assert(temp != NULL);
+    Sentence new_sentence = calloc(1, sizeof(sentence));
+    assert(new_sentence != NULL);
+    new_sentence->string = temp;
+    
+    //If empty intialise "textbuffer", otherwise, add new sentence
+    if (tb->head == NULL) { 
+        tb->head = new_sentence;
+        tb->tail = new_sentence;      
+        tb->no_sentences = 1;        
+    
+    } else {
+        tb->tail->next = new_sentence;
+        new_sentence->prev = tb->tail;
+        tb->tail = new_sentence;
+        tb->no_sentences++;
+    }    
+    
 }
 
 void textbuffer_drop (Textbuffer tb){
-
+    assert(tb->no_sentences > 0);
+    
+    Sentence curr = tb->head;
+    
+    //Free all "sentences" from "textbuffer"
+    while (curr != tb->tail) {
+        curr = curr->next;
+        free(curr->prev->string);        
+        free(curr->prev);
+    }
+    
+    free(tb->tail->string);
+    free(tb->tail);
+    
+    free(tb);    
 }
 
 
+
+static void whitebox_tests (void) {
+    wtest_one();
+    wtest_two();
+    wtest_three();
+}
+
+static void wtest_one (void){
+    printf("Whitebox one element");
+    
+    Textbuffer new = textbuffer_new("Single\n");
+    
+    assert(strcmp(new->head->string, "Single") == 0);
+    assert(new->no_sentences == 1);  
+    assert(new->head == new->tail);
+    
+    assert(new->head->next == NULL);
+    assert(new->head->prev == NULL);
+     
+    puts(" -passed!");
+}
+
+static void wtest_two (void){
+    printf("Whitebox two element");
+    
+    Textbuffer new = textbuffer_new("Single\nDouble\n");
+    
+    assert(new->no_sentences == 2);  
+    assert(new->head == new->tail->prev);
+    assert(new->tail == new->head->next);
+    
+    assert(new->head->prev == NULL);
+    assert(strcmp(new->head->string, "Single") == 0);
+    
+    assert(new->tail->next == NULL);
+    assert(strcmp(new->tail->string, "Double") == 0);
+    
+    puts(" -passed!");
+}
+
+static void wtest_three (void){
+    printf("Whitebox three element");
+    
+    Textbuffer new = textbuffer_new("Single\nDouble\nTriple\n");
+    
+    assert(new->no_sentences == 3);  
+    assert(new->head == new->tail->prev->prev);
+    assert(new->tail == new->head->next->next);
+    
+    assert(new->head->prev == NULL);
+    assert(new->head->next == new->tail->prev->prev);
+    assert(strcmp(new->head->string, "Single") == 0);
+    
+    assert(strcmp(new->head->next->string, "Double") == 0);
+    
+    assert(new->tail->next == NULL);
+    assert(strcmp(new->tail->string, "Triple") == 0);
+    
+    puts(" -passed!");
+}
