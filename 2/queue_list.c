f0438c2f2f7d712572e74c0558172b032af67140
Gavin Wang <z5206647@student.unsw.edu.au>
2018-12-08 11:10:50 +1100

    All tests completed for part 2

--- 2/queue_list.c
+++ 2/queue_list.c
@@ -14,6 +14,10 @@
 #include "queue.h"
 #include "testable.h"
 
+void wtest_single(void);
+void wtest_double(void);
+void wtest_10(void);
+
 typedef struct queue queue;
 typedef struct queue_node queue_node;
 
@@ -52,22 +56,39 @@ void queue_drop (queue *q)
 void queue_en (queue *q, Item it)
 {
 	queue_node *node = queue_node_new (it);
+	
 	if (q->head != NULL) {
-		q->head->next = node;
+		q->tail->next = node;
+		q->tail = node;
+	
 	} else {
 		q->tail = node;
+		q->head = node;	
 	}
-	q->head = node;
+	
+	q->n_items++;
 }
 
 /** Remove an item from the front of a Queue.
  * Sometimes referred to as "dequeue" or "shift". */
 Item queue_de (queue *q)
 {
+    assert(q->n_items > 0);
+    
 	Item it = q->head->item;
-	queue_node *del = q->head;
-	q->head = q->head->next;
-	free (del);
+    queue_node *del = q->head;
+	
+	if (q->n_items == 1) {
+	    q->head = NULL;
+	    q->tail = NULL;
+	
+	} else {
+	    q->head = q->head->next;
+	}
+	
+    free (del);		
+	q->n_items--;
+	
 	return it;
 }
 
@@ -89,4 +110,85 @@ static queue_node *queue_node_new (Item it)
 void white_box_tests (void)
 {
 	// ... you need to write these!
+	wtest_single();
+	wtest_double();
+	wtest_10();
 }
+
+
+void wtest_single(void){
+    puts("White testing single node queue");
+    Queue q = queue_new();
+    Item it = 0;
+    
+    queue_en(q, it);
+    assert(q->head->item == it);
+    assert(q->tail->item == it);
+    assert(q->head->next == NULL);
+    assert(q->n_items == 1);
+   
+    assert(queue_de(q) == 0);
+    assert(q->head == NULL);
+    assert(q->tail == NULL);
+    assert(q->n_items == 0);
+    
+    queue_drop(q);
+}
+void wtest_double(void){
+    puts("White testing double node queue");
+    Queue q = queue_new();
+    Item it = 0;
+    
+    queue_en(q, it);
+
+    it = 1;
+    queue_en(q, it);
+    assert(q->head->item == 0);
+    assert(q->head->next == q->tail);
+    assert(q->tail->item == 1);
+    assert(q->n_items == 2);
+    
+    assert(queue_de(q) == 0);
+    assert(q->head->item == 1);
+    assert(q->tail->item == 1);
+    assert(q->head->next == q->tail->next);
+    assert(q->tail->next == NULL);
+    assert(q->n_items == 1);
+    
+    assert(queue_de(q) == 1);
+    assert(q->head == NULL);
+    assert(q->tail == NULL);
+    assert(q->n_items == 0);
+    
+    queue_drop(q);
+}
+
+void wtest_10(void) {
+    puts("White testing 10 node queue");
+    Queue q = queue_new();
+    
+    int i = 0;
+    Item it = 0;
+    
+    while(i < 10) {
+        queue_en(q, it);
+        it++;
+        i++;
+    }
+    
+    assert(q->head->item == 0);
+    assert(q->tail->item == 9);
+    assert(q->n_items == (size_t) 10);
+    
+    i = 0;   
+    while(i < 10) {
+        assert(queue_de(q) == i);
+        i++;
+    }
+    assert(q->n_items == (size_t) 0);
+    
+    queue_drop(q);
+}
+
+
+
--- 2/test_queue.c
+++ 2/test_queue.c
@@ -12,12 +12,77 @@
 #include "queue.h"
 #include "testable.h"
 
+static void test_single (void);
+static void test_double (void);
+
+static void test_ten (void);
+
 int main (void)
 {
 	white_box_tests ();
-
+    
 	// add more tests of your own!
-
+    test_single();
+    test_double();
+    test_ten();
+                
 	puts ("\nAll tests passed. You are awesome!");
 	return EXIT_SUCCESS;
+}
+
+static void test_single (void) {
+    puts("Testing single node queue");
+    Queue q = queue_new();
+    Item it = 0;
+    
+    queue_en(q, it);
+    assert(queue_size(q) == (size_t) 1);
+    
+    assert(queue_de(q) == 0);
+    assert(queue_size(q) == 0);
+    
+    queue_drop(q);
+}
+
+static void test_double (void) {
+    puts("Testing double node queue");
+    Queue q = queue_new();
+    Item it = 0;
+    
+    queue_en(q, it);
+    it = 1;
+    queue_en(q, it);
+    assert(queue_size(q) == (size_t) 2);
+    
+    assert(queue_de(q) == 0);
+    assert(queue_de(q) == 1);
+    assert(queue_size(q) == 0);
+    
+    queue_drop(q);
+}
+
+
+static void test_ten (void) {
+    puts("Testing 10 nodes queue");
+    Queue q = queue_new();
+    
+    int i = 0;
+    Item it = 0;
+    
+    while(i < 10) {
+        queue_en(q, it);
+        it++;
+        i++;
+    }
+    
+    assert(queue_size(q) == (size_t) 10);
+   
+    i = 0;   
+    while(i < 10) {
+        assert(queue_de(q) == i);
+        i++;
+    }
+    assert(queue_size(q) == (size_t) 0);
+    
+    queue_drop(q);
 }
